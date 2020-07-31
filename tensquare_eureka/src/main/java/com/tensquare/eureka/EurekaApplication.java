68f593e2840fc913902cfac27469a1a5a48efa8e
Nightliuguoxing <2050180797@qq.com>
2020-07-31 23:03:19 +0800

    add eureka

---
+++ tensquare_eureka/src/main/java/com/tensquare/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.tensquare.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName: EurekaApplication
+ * @Author: Samele LGX
+ * @CreateTime: 2020-07-31 22:59
+ * @Description: 服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
